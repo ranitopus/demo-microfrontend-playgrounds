f9a0258974c9a7d2ed6d01321e724ddce3d76f86
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-07-21 02:33:19 -0300

    MF001/start - Making Dev Mode work properly, with watching file changes, rebuild and re-deliver to .static

--- vite-vue-rollup-mfe/host_app/src/main.ts
+++ vite-vue-rollup-mfe/host_app/src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+import App from './App.vue'
+
 const router = createRouter({
   routes: [],
   history: createWebHistory(),
 })
-
-import App from './App.vue'
 
 createApp(App)
   .use(router)
